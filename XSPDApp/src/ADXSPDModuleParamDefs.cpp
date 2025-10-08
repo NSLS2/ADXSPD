6281f5dd7cad4b45f319a4364ea090f385620775
Jakub Wlodek <jwlodek@bnl.gov>
2025-10-08 17:10:31 -0400

    Update paramdefs

--- XSPDApp/src/ADXSPDModuleParamDefs.cpp
+++ XSPDApp/src/ADXSPDModuleParamDefs.cpp
@@ -4,30 +4,22 @@
 #include "ADXSPDModule.h"
 
 void ADXSPDModule::createAllParams() {
-    createParam(ADXSPDModule_ModRotationYawString, asynParamFloat64, &ADXSPDModule_ModRotationYaw);
-    createParam(ADXSPDModule_ModRotationPitchString, asynParamFloat64,
-                &ADXSPDModule_ModRotationPitch);
     createParam(ADXSPDModule_ModBoardTempString, asynParamFloat64, &ADXSPDModule_ModBoardTemp);
-    createParam(ADXSPDModule_ModNumConnectorsString, asynParamInt32,
-                &ADXSPDModule_ModNumConnectors);
-    createParam(ADXSPDModule_ModInterpolationModeString, asynParamInt32,
-                &ADXSPDModule_ModInterpolationMode);
-    createParam(ADXSPDModule_ModPositionXString, asynParamFloat64, &ADXSPDModule_ModPositionX);
-    createParam(ADXSPDModule_ModHumidityString, asynParamFloat64, &ADXSPDModule_ModHumidity);
-    createParam(ADXSPDModule_ModSensorCurrentString, asynParamFloat64,
-                &ADXSPDModule_ModSensorCurrent);
-    createParam(ADXSPDModule_ModFeaturesBitmaskString, asynParamInt32,
-                &ADXSPDModule_ModFeaturesBitmask);
-    createParam(ADXSPDModule_ModSaturationThreshString, asynParamFloat64,
-                &ADXSPDModule_ModSaturationThresh);
-    createParam(ADXSPDModule_ModVoltageString, asynParamFloat64, &ADXSPDModule_ModVoltage);
-    createParam(ADXSPDModule_ModHumidityTempString, asynParamFloat64,
-                &ADXSPDModule_ModHumidityTemp);
-    createParam(ADXSPDModule_ModRotationRollString, asynParamFloat64,
-                &ADXSPDModule_ModRotationRoll);
-    createParam(ADXSPDModule_ModMaxFramesString, asynParamInt32, &ADXSPDModule_ModMaxFrames);
+    createParam(ADXSPDModule_ModInterpModeString, asynParamInt32, &ADXSPDModule_ModInterpMode);
+    createParam(ADXSPDModule_ModRotPitchString, asynParamFloat64, &ADXSPDModule_ModRotPitch);
     createParam(ADXSPDModule_ModNumSubframesString, asynParamInt32, &ADXSPDModule_ModNumSubframes);
-    createParam(ADXSPDModule_ModPositionZString, asynParamFloat64, &ADXSPDModule_ModPositionZ);
-    createParam(ADXSPDModule_ModPositionYString, asynParamFloat64, &ADXSPDModule_ModPositionY);
+    createParam(ADXSPDModule_ModRotRollString, asynParamFloat64, &ADXSPDModule_ModRotRoll);
+    createParam(ADXSPDModule_ModNumConsString, asynParamInt32, &ADXSPDModule_ModNumCons);
+    createParam(ADXSPDModule_ModPosXString, asynParamFloat64, &ADXSPDModule_ModPosX);
+    createParam(ADXSPDModule_ModMaxFramesString, asynParamInt32, &ADXSPDModule_ModMaxFrames);
+    createParam(ADXSPDModule_ModRotYawString, asynParamFloat64, &ADXSPDModule_ModRotYaw);
     createParam(ADXSPDModule_ModFpgaTempString, asynParamFloat64, &ADXSPDModule_ModFpgaTemp);
+    createParam(ADXSPDModule_ModVoltageString, asynParamFloat64, &ADXSPDModule_ModVoltage);
+    createParam(ADXSPDModule_ModFeatBitmaskString, asynParamInt32, &ADXSPDModule_ModFeatBitmask);
+    createParam(ADXSPDModule_ModHumString, asynParamFloat64, &ADXSPDModule_ModHum);
+    createParam(ADXSPDModule_ModHumTempString, asynParamFloat64, &ADXSPDModule_ModHumTemp);
+    createParam(ADXSPDModule_ModPosYString, asynParamFloat64, &ADXSPDModule_ModPosY);
+    createParam(ADXSPDModule_ModSensCurrString, asynParamFloat64, &ADXSPDModule_ModSensCurr);
+    createParam(ADXSPDModule_ModSatThreshString, asynParamFloat64, &ADXSPDModule_ModSatThresh);
+    createParam(ADXSPDModule_ModPosZString, asynParamFloat64, &ADXSPDModule_ModPosZ);
 }
--- XSPDApp/src/ADXSPDModuleParamDefs.h
+++ XSPDApp/src/ADXSPDModuleParamDefs.h
@@ -5,47 +5,47 @@
 // Generated from ADXSPDModule.template
 
 // String definitions for parameters
-#define ADXSPDModule_ModRotationYawString "XSPD_MOD_ROTATION_YAW"
-#define ADXSPDModule_ModRotationPitchString "XSPD_MOD_ROTATION_PITCH"
 #define ADXSPDModule_ModBoardTempString "XSPD_MOD_BOARD_TEMP"
-#define ADXSPDModule_ModNumConnectorsString "XSPD_MOD_NUM_CONNECTORS"
-#define ADXSPDModule_ModInterpolationModeString "XSPD_MOD_INTERPOLATION_MODE"
-#define ADXSPDModule_ModPositionXString "XSPD_MOD_POSITION_X"
-#define ADXSPDModule_ModHumidityString "XSPD_MOD_HUMIDITY"
-#define ADXSPDModule_ModSensorCurrentString "XSPD_MOD_SENSOR_CURRENT"
-#define ADXSPDModule_ModFeaturesBitmaskString "XSPD_MOD_FEATURES_BITMASK"
-#define ADXSPDModule_ModSaturationThreshString "XSPD_MOD_SATURATION_THRESH"
-#define ADXSPDModule_ModVoltageString "XSPD_MOD_VOLTAGE"
-#define ADXSPDModule_ModHumidityTempString "XSPD_MOD_HUMIDITY_TEMP"
-#define ADXSPDModule_ModRotationRollString "XSPD_MOD_ROTATION_ROLL"
-#define ADXSPDModule_ModMaxFramesString "XSPD_MOD_MAX_FRAMES"
+#define ADXSPDModule_ModInterpModeString "XSPD_MOD_INTERP_MODE"
+#define ADXSPDModule_ModRotPitchString "XSPD_MOD_ROT_PITCH"
 #define ADXSPDModule_ModNumSubframesString "XSPD_MOD_NUM_SUBFRAMES"
-#define ADXSPDModule_ModPositionZString "XSPD_MOD_POSITION_Z"
-#define ADXSPDModule_ModPositionYString "XSPD_MOD_POSITION_Y"
+#define ADXSPDModule_ModRotRollString "XSPD_MOD_ROT_ROLL"
+#define ADXSPDModule_ModNumConsString "XSPD_MOD_NUM_CONS"
+#define ADXSPDModule_ModPosXString "XSPD_MOD_POS_X"
+#define ADXSPDModule_ModMaxFramesString "XSPD_MOD_MAX_FRAMES"
+#define ADXSPDModule_ModRotYawString "XSPD_MOD_ROT_YAW"
 #define ADXSPDModule_ModFpgaTempString "XSPD_MOD_FPGA_TEMP"
+#define ADXSPDModule_ModVoltageString "XSPD_MOD_VOLTAGE"
+#define ADXSPDModule_ModFeatBitmaskString "XSPD_MOD_FEAT_BITMASK"
+#define ADXSPDModule_ModHumString "XSPD_MOD_HUM"
+#define ADXSPDModule_ModHumTempString "XSPD_MOD_HUM_TEMP"
+#define ADXSPDModule_ModPosYString "XSPD_MOD_POS_Y"
+#define ADXSPDModule_ModSensCurrString "XSPD_MOD_SENS_CURR"
+#define ADXSPDModule_ModSatThreshString "XSPD_MOD_SAT_THRESH"
+#define ADXSPDModule_ModPosZString "XSPD_MOD_POS_Z"
 
 // Parameter index definitions
-int ADXSPDModule_ModRotationYaw;
-int ADXSPDModule_ModRotationPitch;
 int ADXSPDModule_ModBoardTemp;
-int ADXSPDModule_ModNumConnectors;
-int ADXSPDModule_ModInterpolationMode;
-int ADXSPDModule_ModPositionX;
-int ADXSPDModule_ModHumidity;
-int ADXSPDModule_ModSensorCurrent;
-int ADXSPDModule_ModFeaturesBitmask;
-int ADXSPDModule_ModSaturationThresh;
-int ADXSPDModule_ModVoltage;
-int ADXSPDModule_ModHumidityTemp;
-int ADXSPDModule_ModRotationRoll;
-int ADXSPDModule_ModMaxFrames;
+int ADXSPDModule_ModInterpMode;
+int ADXSPDModule_ModRotPitch;
 int ADXSPDModule_ModNumSubframes;
-int ADXSPDModule_ModPositionZ;
-int ADXSPDModule_ModPositionY;
+int ADXSPDModule_ModRotRoll;
+int ADXSPDModule_ModNumCons;
+int ADXSPDModule_ModPosX;
+int ADXSPDModule_ModMaxFrames;
+int ADXSPDModule_ModRotYaw;
 int ADXSPDModule_ModFpgaTemp;
+int ADXSPDModule_ModVoltage;
+int ADXSPDModule_ModFeatBitmask;
+int ADXSPDModule_ModHum;
+int ADXSPDModule_ModHumTemp;
+int ADXSPDModule_ModPosY;
+int ADXSPDModule_ModSensCurr;
+int ADXSPDModule_ModSatThresh;
+int ADXSPDModule_ModPosZ;
 
-#define ADXSPDMODULE_FIRST_PARAM ADXSPDModule_ModRotationYaw
-#define ADXSPDMODULE_LAST_PARAM ADXSPDModule_ModFpgaTemp
+#define ADXSPDMODULE_FIRST_PARAM ADXSPDModule_ModBoardTemp
+#define ADXSPDMODULE_LAST_PARAM ADXSPDModule_ModPosZ
 
 #define NUM_ADXSPDMODULE_PARAMS ((int) (&ADXSPDMODULE_LAST_PARAM - &ADXSPDMODULE_FIRST_PARAM + 1))
 
