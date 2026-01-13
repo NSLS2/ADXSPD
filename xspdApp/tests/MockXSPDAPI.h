10f64baa8acb8829d79e976c752efb28db143e39
Jakub Wlodek <jwlodek.dev@gmail.com>
2026-01-12 21:38:23 -0500

    Remove accidentally commited merge artifacts

--- xspdApp/tests/MockXSPDAPI.h
+++ xspdApp/tests/MockXSPDAPI.h
@@ -6,13 +6,8 @@
 #include "XSPDAPI.h"
 
 class MockXSPDAPI : public XSPD::API {
-<<<<<<< Updated upstream
    public:
     MockXSPDAPI() : XSPD::API("localhost", 8080){};
-=======
-    public:
-    MockXSPDAPI() : XSPD::API("http://localhost", 8080) {};
->>>>>>> Stashed changes
     MOCK_METHOD(json, SubmitRequest, (string uri, XSPD::RequestType reqType), (override));
     MOCK_METHOD(string, GetApiVersion, (), (override));
 };
